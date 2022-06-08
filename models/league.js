91f20f1453d62f0d594449c051104f8d6a06aeaa
Camay Duran <11camayduran@gmail.com>
2022-06-08 21:22:29 +0300

    rehber app

---
+++ models/league.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class league extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  league.init({
+    name: DataTypes.STRING,
+    surname: DataTypes.STRING,
+    company: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'league',
+  });
+  return league;
+};
